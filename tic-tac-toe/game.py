70ebd7592669976076a0d72026bc02faab9e1be3
Ayca Gökdağ <77059420+aycagokdag@users.noreply.github.com>
2021-09-09 21:16:18 +0300

    update tic-tac-toe

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -47,18 +47,28 @@ class TicTacToe:
             return True
         return False
     
-    
+    # runs after each move
+    # if there is a winner, returns True
     def winner(self, square, letter):
+        # check rows
+        # index:0 ... self.board[0:3] ... 0,1,2
+        # index:1 ... self.board[3:6] ... 3,4,5
+        # index:2 ... self.board[6:9] ... 6,7,8
         row_ind = square // 3 # result without decimal part
         row = self.board[row_ind*3 : (row_ind + 1) * 3]
         if all([spot == letter for spot in row]):
             return True
         
+        # check columns
+        # index:0 ... self.board[0->3->6]
+        # index:1 ... self.board[1->4->7]
+        # index:2 ... self.board[2->5->8]
         col_ind = square % 3
         column = [self.board[col_ind + i * 3] for i in range(3)]
         if all([spot == letter for spot in column]):
             return True
         
+        # check diagonals    
         if square % 2 == 0:
             diagonal1 = [self.board[i] for i in [0, 4, 8]]
             if all([spot == letter for spot in diagonal1]):
@@ -75,6 +85,7 @@ def play(game, x_player, o_player, print_game=True):
     if print_game:
         game.print_board_nums()
     
+    # first move is always X
     letter = 'X'
     while game.empty_squares():
         if letter == 'O':
@@ -97,6 +108,7 @@ def play(game, x_player, o_player, print_game=True):
             # after a move, we need to alternate letters
             letter = 'O' if letter == 'X' else 'X'
         
+        # puts some time interval between the moves
         time.sleep(.9)
     
     if print_game:
@@ -105,7 +117,12 @@ def play(game, x_player, o_player, print_game=True):
         
 
 if __name__ == '__main__':
-    x_player = HumanPlayer('X')
-    o_player = ComputerPlayer('O')
+    # players can assign differently 
+    x_player = HumanPlayer('X') # X player will be human
+    o_player = ComputerPlayer('O') # O player will be computer
+    
     t = TicTacToe()
+    # print_game property automatically assigned to True,
+    # that is going to be helpful while making computer play with itself 
+    # and just counting the wins without printing the game for the sake of probability 
     play(t, x_player, o_player, print_game=True)
